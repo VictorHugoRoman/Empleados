da4379572a8b069319e95bbdedcf6329d7d1a48b
ViGaFamm <ViGaFamm@DESKTOP-RLTKBPD>
2017-10-26 01:31:04 -0500

    Agregar archivos de proyecto.

---
+++ Empleados/Models/Departamentos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Empleados.Models
+{
+    public class Departamentos
+    {
+        [Key]
+        public int puesto { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string descripcion { get; set; }
+        //public Empleados empleado { get; set; }
+        public virtual ObservableCollection<Empleados> empleados { get; set; }
+    }
+}
---
+++ Empleados/Models/Empleados.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Empleados.Models
+{
+    public class Empleados
+    {
+        [Key]
+        public int Clave_Emp { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [Display(Name = "Nombre")]
+        public string nombre { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [Display(Name = "Apellido Paterno")]
+        public string apPaterno { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [Display(Name = "Apellido Materno")]
+        public string apMaterno { get; set; }
+        [Required]
+        [Display(Name = "Fecha Nacimiento")]
+        public DateTime FecNac { get; set; }
+        //[Required]
+        //[Display(Name = "Departamento")]
+       // public int departamento { get; set; }
+        [Required]
+        [Display(Name = "Sueldo")]
+        public decimal sueldo { get; set; }
+
+        public virtual Departamentos departamento { get; set; }
+        public IEnumerable<Departamentos> ListDepas{ get; set; }
+
+    }
+}
